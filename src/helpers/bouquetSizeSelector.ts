3e25598bf80e774fb39e130f0bcdf6dedb27565b
Aykut Alpgiray Ateş <alpgirayaykut@gmail.com>
2018-12-16 17:18:55 +0300

    placed comments to the  bouquetSizeSelector

--- src/helpers/bouquetSizeSelector.ts
+++ src/helpers/bouquetSizeSelector.ts
@@ -2,14 +2,25 @@ export enum BouquetSize {
   S = "S",
   L = "L"
 }
-
+/**
+ * Extracts size of bouquet from bouquet specification.
+ * @param spec Bouquet specification
+ * @returns Size of bouquet
+ */
 export default function bouquetSizeSelector(spec: string): BouquetSize {
+  /**
+   * Bouquet name must be single upper case letter
+   * Bouquet size must fallow bouquet name and must be capital letter S or L
+   */
   const expression = /^([A-Z])(S|L).+/;
 
   if (!spec.match(expression)) {
     throw new Error("Specification is faulty");
   }
-
+  /**
+   * Bouquet size is either S or L, this is tested via regular expression.
+   * Therefore, type cast to enum BouquetSize should be safe.
+   */
   const bouquetSize: BouquetSize = spec.replace(
     expression,
     "$2"
