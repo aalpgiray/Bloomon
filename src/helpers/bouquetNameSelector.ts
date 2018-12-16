68a49b8bf89c5d611f8cad2f9e38668e0a26277f
Aykut Alpgiray Ateş <alpgirayaykut@gmail.com>
2018-12-16 17:19:29 +0300

    placed comments to the bouquetNameSelector

--- src/helpers/bouquetNameSelector.ts
+++ src/helpers/bouquetNameSelector.ts
@@ -1,4 +1,13 @@
+/**
+ * Extracts name of bouquet from bouquet specification.
+ * @param spec Bouquet specification
+ * @returns Name of bouquet
+ */
 export default function bouquetNameSelector(spec: string): string {
+  /**
+   * Bouquet name must be single upper case letter
+   * Bouquet size must fallow bouquet name. However, this control migth not be required here.
+   */
   const expression = /^([A-Z])(S|L).+/;
 
   if (!spec.match(expression)) {
